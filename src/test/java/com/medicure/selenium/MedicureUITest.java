221b9477c05540dc6584db2ad8cdc6a81c384227
vedprakash sharma <160297817+vedprakash0708@users.noreply.github.com>
2025-03-29 20:41:59 +0100

    Update MedicureUITest.java

--- src/test/java/com/medicure/selenium/MedicureUITest.java
+++ src/test/java/com/medicure/selenium/MedicureUITest.java
@@ -1,5 +1,13 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class MedicureUITest {
-    WebDriver driver;
+    private WebDriver driver;
     
     @Before
     public void setup() {
@@ -12,11 +20,13 @@ public class MedicureUITest {
         driver.get("http://medicure-service/doctors");
         driver.findElement(By.id("searchInput")).sendKeys("John");
         driver.findElement(By.id("searchButton")).click();
-        // Assertions
+        // TODO: Add assertions to verify search results
     }
     
     @After
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
