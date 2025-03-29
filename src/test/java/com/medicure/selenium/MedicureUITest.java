faa616044acfd83465744bce1475b7ae79e70c13
vedprakash sharma <160297817+vedprakash0708@users.noreply.github.com>
2025-03-29 20:51:55 +0100

    Update MedicureUITest.java

--- src/test/java/com/medicure/selenium/MedicureUITest.java
+++ src/test/java/com/medicure/selenium/MedicureUITest.java
@@ -1,32 +1,36 @@
-import org.junit.Before;
+package com.medicure.selenium;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MedicureUITest {
-    private WebDriver driver;
-    
+    WebDriver driver;
+
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Headless mode for Jenkins
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
     }
-    
+
     @Test
     public void testDoctorSearch() {
         driver.get("http://medicure-service/doctors");
         driver.findElement(By.id("searchInput")).sendKeys("John");
         driver.findElement(By.id("searchButton")).click();
-        // TODO: Add assertions to verify search results
     }
-    
+
     @After
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 }
